cadde68c282f722d31f9067b8471c097704246cd
Corin Finnin <finninc@student.waihekhigh.school.nz>
2021-02-04 04:16:45 +1300

    test commit

---
+++ Platformer.py
@@ -0,0 +1,15 @@
+import arcade
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+SCREEN_TITLE = "Platformer"
+
+
+
+def main():
+    arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT,SCREEN_TITLE)
+
+    arcade.run()
+
+
+main()
